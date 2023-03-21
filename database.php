80e92dfa9cc763a3b1ae93fd8309404f2623315d
Yigit Uzel <pcdt911@gmail.com>
2023-03-21 11:17:35 +0100

    database.php toegevoegd

---
+++ database.php
@@ -0,0 +1,8 @@
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "GFG";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
