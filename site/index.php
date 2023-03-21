d6eba88e2c1aa94e84049ac7a81bfd730ed251d3
Yigit Uzel <pcdt911@gmail.com>
2023-03-21 13:31:57 +0100

    buttons gemaakt

--- site/index.php
+++ site/index.php
@@ -1,8 +1,3 @@
-<?php
-// Dit is het startpunt van je applicatie.
-echo 'Welkom bij het receptenboek';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,8 +5,24 @@ echo 'Welkom bij het receptenboek';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css" />
 </head>
 <body>
-<p>aaa</p>
+
+<div class="taskbar">
+  <div class="left-buttons">
+    <button>knop 1</button>
+    <button>knop 2</button>
+    <button>knop 3</button>
+  </div>
+  <div class="logo">
+    <h1>Food Tech</h1>
+  </div>
+  <div class="right-buttons">
+    <button>knop 4</button>
+    <button>knop 5</button>
+  </div>
+</div>
+
 </body>
 </html>
