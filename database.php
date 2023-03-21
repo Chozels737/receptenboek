127d4097629fbc842f74bfcc83c7b039f1e58e70
Yigit Uzel <pcdt911@gmail.com>
2023-03-21 12:41:44 +0100

    database.php gemaakt en werkt

--- database.php
+++
@@ -1,8 +0,0 @@
-// Database configuratie
-$host  = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "GFG";
-
-// Maak een  database connectie
-$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
