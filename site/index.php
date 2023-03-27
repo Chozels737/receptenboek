8776e7ccc4f99f680e0d1a751aefd8d3d7fc6382
Yigit Uzel <pcdt911@gmail.com>
2023-03-27 10:54:27 +0200

    heb de website gefixt nadat ik een error had

--- site/index.php
+++ site/index.php
@@ -4,18 +4,10 @@ require "database.php";
 
 
 //de sql query
-$sql = "SELECT * FROM recepten";
+$sql = "SELECT * FROM lebanese_keuken";
 
 //hier wordt de query uitgevoerd met de database
 $result = mysqli_query($conn, $sql);
-
-/**
- * Hier wordt het resultaat ($result) omgezet
- * in een *multidimensionale associatieve array
- * in dit voorbeeld staat $all_users maar dit mag
- * voor bijvoorbeeld producten $all_products heten.
- * Maar dit kies je zelf
- */
 
 $all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
@@ -27,12 +19,12 @@ $all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
     <header>
         <img class="logo" src="/images/logo.png" />
-        <nav>
+        <nav> 
           <ul class="nav__links">
             <li><a href="index.php">contact</a></li>
             <li><a href="index.php">contact</a></li>
@@ -41,14 +33,17 @@ $all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
         </nav>
         <a class="cta" href="#"><button>Contact</button></a>
       </header> 
+      <img class="banner" src="/images/banner1.png" />
 <div class="flex-container">
-  <div>1<img class="logo" src="/images/logo.png" /></div>
-  <div>2<img class="logo" src="/images/logo.png" /></div>
-  <div>3<img class="logo" src="/images/logo.png" /></div>  
-</div>
-<div class="flex-container">
-<div>1<img class="logo" src="/images/logo.png" /></div>
-<div>2<img class="logo" src="/images/logo.png" /></div>
-</div>
+    
+  <?php foreach ($all_recepten as $recept) : ?>
+
+    <div class="recept-blok">
+      <h3>Hier een titel</h3>
+      <img src="images/<?php echo $recept["plaatje"] ?>">
+  </div>
+
+  <?php endforeach  ?>
+
 </body>
 </html>
