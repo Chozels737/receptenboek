07fc7d974855498f668ec6e5622f13b8bc7c8372
Yigit Uzel <pcdt911@gmail.com>
2023-03-30 13:47:32 +0200

    nav en footer in recept pagina

--- site/recept.php
+++ site/recept.php
@@ -18,9 +18,20 @@ $recept = mysqli_fetch_assoc($result);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/footer.css" />  
 </head>
 <body>
+<header>
+        <img class="logo" src="/images/logo.png" />
+        <nav>
+          <?php include 'nav.php'?>
+        </nav>
+    </header> 
     <?php echo $recept['naam'] ?>
     <img src="images/<?php echo $recept["plaatje"] ?>">
 </body>
+<footer>
+<?php include 'footer.php'?>
+</footer>
 </html>
